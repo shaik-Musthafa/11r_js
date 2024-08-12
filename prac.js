4825855094c77a7c6109312d4cfec06dbffb4449
shaik-Musthafa <musthafaskkrnl@gmail.com>
2024-08-12 14:26:57 +0530

    practise

--- prac.js
+++ prac.js
@@ -22,3 +22,58 @@
 // if(x<y){
 //   console.log("")
 // }
+//09/09/25
+// for loop
+// write a program to print the square of each number 1 to 10
+// let j=1;
+// for (i=1;i<=10;i++){
+//     j = i**2
+//     console.log(j);
+// }
+// write a program to find the sum of  all nums from 1 to 20 that are divisible by 3
+// let sum = 0;
+// for(i=1;i<=20;i++){
+//     if(i%3==0){
+//     console.log(i)
+//     sum = sum + i
+//     console.log(`sum of  all nums from 1 to 20  are ${sum}`);
+// }
+// }
+//write  a program to print the cube of each number from 1 -15
+// let sum =0;
+// for(i=1;i<=15;i++){
+//     console.log(i)
+//     sum = i**3;
+//     console.log(`cube of ${i} is ${sum}`);
+
+// }
+//do-while loop 
+//1. write a program to print the numbers from 10 to 1 in reverse order 
+// let i=10;
+// do{
+//     console.log(i);
+//     --i
+// }while(i>=1)
+//2.write a program to poduct all numbers from 1 -12
+// let i = 1;
+// sum=1;
+// do{
+//     console.log(i);
+//     i++;
+//     sum = sum*i
+//     console.log(sum);
+// }while(i<12)
+//3.write a program to print the numbers from 20 to 1 in reverse in order, but only print every other number
+// let i=20;
+// do{
+//     i%2==0
+//     console.log(i)
+// }while(i=i-1)
+// while loop
+//1.write a program to print the numbers from 1 to 20, but skip numbers that are divisible by 5.
+let i = 1;
+while(i<=20){
+    
+}
+//2.write a program to find the sum of all numbers from 1 - 30 that are divisible by 4
+//3.write a program to print the numbers from 50 -1 in reverse order, but only print numbers that are divisible by3
