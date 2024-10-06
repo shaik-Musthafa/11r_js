c3ca6ecbdaf938a532893042f3ed75242c1a830e
shaik-Musthafa <musthafaskkrnl@gmail.com>
2024-10-06 22:46:46 +0530

    NEW commit

---
+++ ps-task.js/alphapartner.js
@@ -0,0 +1,23 @@
+rows=+prompt("enter rows: ")
+for(i=0;i<rows;i++){
+    res="",alpha=65
+    for(j=1;j<=rows-i;j++){
+        res+=String.fromCharCode(alpha)
+        alpha++
+    }
+    if(i==0){
+        console.log(res+res.slice(0,res.length-1).split("").reverse().join(""))
+    }else{
+        console.log(res+" ".repeat(2*i-1)+res.split("").reverse().join(""))
+    }
+}
+// node /tmp/MCFRGVvgKC.js
+// enter rows: 7
+// ABCDEFGFEDCBA
+// ABCDEF FEDCBA
+// ABCDE   EDCBA
+// ABCD     DCBA
+// ABC       CBA
+// AB         BA
+// A           A
+
